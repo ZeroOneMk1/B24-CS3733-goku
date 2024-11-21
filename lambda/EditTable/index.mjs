6616e5485138c2c11b3c3671f443f8833d33e922
notsoli <samrandaofficial@gmail.com>
2024-11-21 13:12:04 -0500

    Added authentication to the EditTable function

--- lambda/EditTable/index.mjs
+++ lambda/EditTable/index.mjs
@@ -1,6 +1,18 @@
-import { connect } from "../opt/lib/connect.mjs"
+import { connect } from "../opt/lib/connect.mjs";
+import { verify } from "../opt/lib/verify.mjs";
 
 export const handler = async (event) => {
+  const { decoded, error: authError } = verify(event.jwt);
+  if (authError) return {
+    statusCode: 401,
+    error: "Unauthorized"
+  }
+
+  if (decoded.isAdmin) return {
+    statusCode: 401,
+    error: "Administrators cannot modify restaurant information"
+  }
+
   // create database connection
   const { pool, error: dbError } = await connect();
   if (dbError) return {
@@ -8,16 +20,11 @@ export const handler = async (event) => {
     error: "Unable to create database connection"
   };
 
-  // hardcode restaurantID
-  const restaurantID = 'fafd52f9-9529-4eb2-be00-8dbc580221af'; //correct
-  // const restaurantID = 'fafd52f9-9529-4eb2-be00-8dbc580221ad'; // incorrect
-
   let nextNumber;
-
   try {
     // insert new table
     const [editTableResults, _editTableFields] = await pool.execute("update tables set seats = ? where number = ? and restaurantID = ?", [
-      event.seats, event.number, restaurantID
+      event.seats, event.number, decoded.restaurantID
     ]);
 
     if (editTableResults.affectedRows == 0) {
@@ -36,7 +43,7 @@ export const handler = async (event) => {
 
     // select all seats and return to users
     const [selectTablesResults, _selectTablesFields] = await pool.execute(
-      "select number, seats from tables where restaurantID = ? ", [restaurantID]);
+      "select number, seats from tables where restaurantID = ? ", [decoded.restaurantID]);
     return {
       statusCode: 200,
       body: JSON.stringify({ tables: selectTablesResults }),
