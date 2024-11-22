e3d20d6f6b51a7f89c4a60ce3da7a23a8e122c42
notsoli <samrandaofficial@gmail.com>
2024-11-21 22:00:21 -0500

    Added authentication

---
+++ tables4u/src/middleware.ts
@@ -0,0 +1,51 @@
+import { NextResponse } from 'next/server';
+import type { NextRequest } from 'next/server';
+import { jwtVerify } from 'jose';
+import type { JwtPayload } from './lib/jwtPayload';
+
+const protectedRoutes = ['/manage'];
+
+export async function middleware(request: NextRequest) {
+    const path = request.nextUrl.pathname
+    const isProtectedRoute = protectedRoutes.includes(path)
+
+    if (isProtectedRoute) {
+        // grab token and check if it exists
+        const token = request.cookies.get("jwt")?.value;
+        if (!token) return NextResponse.redirect(new URL('/owner-login', request.url));
+
+        try {
+            // attempt to verify token
+            const secret = new TextEncoder().encode(process.env.JWT_SECRET);
+            const { payload, protectedHeader: _protectedHeader } = await jwtVerify(token, secret);
+            const decoded = payload as JwtPayload;
+
+            // jwt is valid
+            console.log(decoded);
+            return NextResponse.next();
+        } catch(error) {
+            // jwt is invalid, determine if user was previously an owner or admin
+            const decoded = JSON.parse(atob(token.split(".")[1])) as JwtPayload;
+            
+            // redirect to correct login
+            const redirectURL = (decoded.isAdmin) ? "/admin-login" : "/owner-login"
+            const response = NextResponse.redirect(new URL(redirectURL, request.url));
+            response.cookies.delete("jwt");
+            return response;
+        }
+    }
+
+    return NextResponse.next();
+}
+
+export const config = {
+    matcher: [
+      /*
+       * Match all request paths except for the ones starting with:
+       * - _next/static (static files)
+       * - _next/image (image optimization files)
+       * - favicon.ico, sitemap.xml, robots.txt (metadata files)
+       */
+      '/((?!_next/static|_next/image|favicon.ico|sitemap.xml|robots.txt).*)',
+    ],
+}
