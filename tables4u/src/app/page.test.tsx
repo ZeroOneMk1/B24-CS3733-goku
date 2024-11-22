1543c400ce58744fbd0e32ebe7833d562c32c1c5
notsoli <samrandaofficial@gmail.com>
2024-11-21 22:00:26 -0500

    Moved test

---
+++ tables4u/src/app/page.test.tsx
@@ -0,0 +1,7 @@
+import { render } from '@testing-library/react'
+import { expect, test } from 'vitest'
+import Home from './page' // Adjust the import path as necessary
+
+test('Home', async () => {
+  const { container } = render(<Home />)
+});
