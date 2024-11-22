d68931dd0cccb3486146718525f99f15ac888d18
notsoli <samrandaofficial@gmail.com>
2024-11-22 14:08:32 -0500

    Started restaurant details panel

--- tables4u/src/app/manage/page.tsx
+++ tables4u/src/app/manage/page.tsx
@@ -1,5 +1,24 @@
+import { useState } from "react";
+
 export default function Manage() {
+    const [ restaurantInfo, setRestaurantInfo] = useState({
+        name: "Chashu Ramen + Izakaya",
+        address: "271 Main St.",
+        isActive: false,
+        openingTime: 13,
+        closingTime: 22,
+    });
     return (
-        <h1>Manage</h1>
+        <div id="restaurant-details">
+            <h1>Restaurant Details</h1>
+            <div id="basic-information">
+                <p>Restaurant is <em>{restaurantInfo.isActive ? "active" : "inactive"}</em></p>
+                <button>Activate</button>
+                <p>Name: {restaurantInfo.name}</p>
+                <p>Address: {restaurantInfo.address}</p>
+                <p>Opening time: {restaurantInfo.openingTime}</p>
+                <p>Closing time: {restaurantInfo.closingTime}</p>
+            </div>
+        </div>
     );
 }
