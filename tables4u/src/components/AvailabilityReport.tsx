32208392beffae22d4b0deec2e3d224a3ec5e192
Renata Kaplan <renkap7747@gmail.com>
2024-12-08 14:46:10 -0500

    Working on it more :p

--- tables4u/src/components/AvailabilityReport.tsx
+++ tables4u/src/components/AvailabilityReport.tsx
@@ -5,6 +5,8 @@ import styles from "./AvailabilityReport.module.css";
 
 export default function AvailabilityReport({restaurantID}: {restaurantID?: string}) {
     
+    const [generateStatus, setGenerateStatus] = useState("success");
+
     const [startDate, setStartDate] = useState(() => {
         const date = new Date();
         return `${date.getFullYear()}-${(date.getMonth()+1).toString().padStart(2, "0")}-${(date.getDay()+1).toString().padStart(2, "0")}`;
@@ -26,14 +28,16 @@ export default function AvailabilityReport({restaurantID}: {restaurantID?: strin
             </div>
             <div id={styles.dateForm}>
                 <form onSubmit={generateAvailabityReport}>
-                    <label htmlFor="startDate">Starting day:</label>
+                    <label htmlFor="startDate">Starting day: </label>
                     <input type="date" name="startDate" id="startDate"
                         required value={startDate} onChange={(e) => setStartDate(e.target.value)} />
-                    <label htmlFor="endDate">Ending day (inclusive):</label>
+                    <label htmlFor="endDate">Ending day (inclusive): </label>
                     <input type="date" name="endDate" id="endDate"
                         required value={endDate} onChange={(e) => setEndDate(e.target.value)} />
-                    
+                    <input type="submit" value={generateStatus == "waiting" ? "Loading..." : "Generate"} />
                 </form>
+                {generateStatus !== "waiting" && generateStatus !== "success" &&
+                    <p>Error: {generateStatus}</p>}
             </div>
         </div>
     )
