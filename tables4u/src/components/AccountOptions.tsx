2c01471a80f49704180a184f47dd30b0ef8cc48d
notsoli <samrandaofficial@gmail.com>
2024-12-08 10:19:07 -0500

    Moved a bunch of components to a new directory

--- tables4u/src/components/AccountOptions.tsx
+++ tables4u/src/components/AccountOptions.tsx
@@ -5,8 +5,9 @@ import { RestaurantInfo } from './contexts';
 
 import styles from './AccountOptions.module.css'
 
-export default function AccountOptions({ restaurantInfo }: {
-    restaurantInfo: RestaurantInfo
+export default function AccountOptions({ restaurantInfo, restaurantID }: {
+    restaurantInfo: RestaurantInfo,
+    restaurantID?: string
 }) {
     const router = useRouter();
     const [ deleteRestaurantStatus, setDeleteRestaurantStatus ] = useState(" ");
@@ -20,7 +21,8 @@ export default function AccountOptions({ restaurantInfo }: {
         setDeleteRestaurantStatus("Deleting...");
         const url = process.env.NEXT_PUBLIC_FUNCTION_URL + "/DeleteRestaurant";
         const body = JSON.stringify({
-            jwt: document.cookie.match(new RegExp(`(^| )jwt=([^;]+)`))?.at(2),
+            restaurantID: restaurantID ?? null,
+            jwt: document.cookie.match(new RegExp(`(^| )jwt=([^;]+)`))?.at(2)
         });
 
         const response = await fetch(url, { method: "POST", body });
