6dc49f017eccd125830daee03503194a7371f144
notsoli <samrandaofficial@gmail.com>
2024-12-08 09:50:57 -0500

    Moved "Delete Restaurant" and "Logout" into a component for reuse

---
+++ tables4u/src/app/manage-restaurant/AccountOptions.tsx
@@ -0,0 +1,50 @@
+'use client';
+import { useRouter } from 'next/navigation';
+import { useState } from "react";
+import { RestaurantInfo } from './contexts';
+
+import styles from './AccountOptions.module.css'
+
+export default function AccountOptions({ restaurantInfo }: {
+    restaurantInfo: RestaurantInfo
+}) {
+    const router = useRouter();
+    const [ deleteRestaurantStatus, setDeleteRestaurantStatus ] = useState(" ");
+    const isDisabled = deleteRestaurantStatus == "Deleting...";
+
+    async function deleteRestaurant() {
+        // create confirm dialog
+        if (!window.confirm(`Are you sure you want to delete ${restaurantInfo.name}?\n
+            This action cannot be undone.`)) return;
+
+        setDeleteRestaurantStatus("Deleting...");
+        const url = process.env.NEXT_PUBLIC_FUNCTION_URL + "/DeleteRestaurant";
+        const body = JSON.stringify({
+            jwt: document.cookie.match(new RegExp(`(^| )jwt=([^;]+)`))?.at(2),
+        });
+
+        const response = await fetch(url, { method: "POST", body });
+        const result = await response.json();
+
+        if (result.statusCode == 200) {
+            // set status, delete cookie, and return to index
+            setDeleteRestaurantStatus("Deleted, redirecting...");
+            document.cookie = "jwt=;";
+            setTimeout(() => router.push("/"), 2000);
+        } else setDeleteRestaurantStatus(result.error);
+    }
+
+    function logout() {
+        document.cookie = "jwt=;";
+        router.push("/");
+    }
+    
+    return (
+        <div id={styles.accountOptions}>
+            <h2>Account Options</h2>
+            <button onClick={deleteRestaurant} disabled={isDisabled}>Delete restaurant</button>
+            <p>{deleteRestaurantStatus}</p>
+            <button onClick={logout}>Logout</button>
+        </div>
+    )
+}
