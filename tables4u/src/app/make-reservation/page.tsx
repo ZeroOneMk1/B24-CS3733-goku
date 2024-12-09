9486a73d648fabd10c51bce95d5069e427cbb230
notsoli <samrandaofficial@gmail.com>
2024-12-08 19:34:28 -0500

    Refactored reservation pages

--- tables4u/src/app/make-reservation/page.tsx
+++ tables4u/src/app/make-reservation/page.tsx
@@ -1,7 +1,6 @@
 "use client";
 
-import React, { useEffect, useState, Suspense } from 'react';
-import { useSearchParams } from 'next/navigation';
+import React, { useEffect, useState } from 'react';
 
 import ReservationInfo from '../(reservations)/ReservationInfo';
 
@@ -30,15 +29,14 @@ interface GetRestaurantInfoResponse {
     }[];
 }
 
-const MakeReservation: React.FC = () => {
-    const searchParams = useSearchParams();
-    const restaurantID = searchParams.get('restaurantID');
+export default function MakeReservation({ searchParams }: { searchParams: { restaurantID: string}}) {
+    const restaurantID = searchParams.restaurantID;
 
     const [name, setName] = useState<string>('');
     const [email, setEmail] = useState<string>('');
     const [time, setTime] = useState<string>('');
     const [restaurant, setRestaurant] = useState<{ name: string; address: string }>({ name: '', address: '' });
-    const [restaurantInfo, setRestaurantInfo] = useState<GetRestaurantInfoResponse>({ restaurantInfo: { restaurantID: '', name: '', address: '', isActive: 0, openingTime: 0, closingTime: 0, credentialID: '' }, tables: [] });
+    const [restaurantInfo, setRestaurantInfo] = useState<GetRestaurantInfoResponse | null>(null);
 
     // run getAvailableTimes when date or guestCount changes
     const [guestCount, setGuestCount] = useState("1");
@@ -46,8 +44,9 @@ const MakeReservation: React.FC = () => {
     const [availableTimes, setAvailableTimes] = useState<string[]>([]);
     const [loading, setLoading] = useState<boolean>(false);
     const [message, setMessage] = useState<string>("");
-    const [hasReserved, setHasReserved] = useState<boolean>(false);
     const [reservationCode, setReservationCode] = useState<number>(0);
+
+    let hasReserved = !!reservationCode;
 
     const getAvailableTimes = async (day: string, guestCount: number) => {
         setLoading(true);
@@ -55,74 +54,53 @@ const MakeReservation: React.FC = () => {
         setTime('');
         console.log('Getting available times for', day, 'with', guestCount, 'guests');
         console.log('Restaurant Info:', restaurantInfo);
-        const fetchRestaurantInfo = async () => {
-            try {
-                const openingTime = restaurantInfo.restaurantInfo.openingTime;
-                const closingTime = restaurantInfo.restaurantInfo.closingTime;
-                const times: string[] = [];
-                for (let i = openingTime; i < closingTime; i++) {
-                    const response = await fetch(process.env.NEXT_PUBLIC_FUNCTION_URL + "/ListRestaurants", {
-                        method: "POST",
-                        headers: {
-                            "Content-Type": "application/json",
-                        },
-                        body: JSON.stringify({ filters: { name: restaurant.name, date: day, time: i.toString(), guestCount: guestCount.toString(), onlyShowAvailableRestaurants: 'true' } }),
-                    });
-                    const data = await response.json();
-                    if (data.restaurants && data.restaurants.length > 0) {
-                        times.push(i.toString());
-                    }
-                }
-                setAvailableTimes(times);
-                if (times.length === 0) {
-                    setMessage("No available times found");
-                } else {
-                    setMessage("");
-                }
-            } catch (error) {
-                console.error('Error fetching restaurant info:', error);
-                setMessage("Error fetching available times");
-            } finally {
-                setLoading(false);
-            }
-        };
+        
+        if (!restaurantInfo) return;
+        
+        const openingTime = restaurantInfo.restaurantInfo.openingTime;
+        const closingTime = restaurantInfo.restaurantInfo.closingTime;
+        const times: string[] = [];
+        for (let i = openingTime; i < closingTime; i++) {
+            const response = await fetch(process.env.NEXT_PUBLIC_FUNCTION_URL + "/ListRestaurants", {
+                method: "POST",
+                body: JSON.stringify({ filters: { name: restaurant.name, date: day, time: i.toString(),
+                    guestCount: guestCount.toString(), onlyShowAvailableRestaurants: 'true' } }),
+            });
 
-        if (day && guestCount) {
-            await fetchRestaurantInfo();
-            console.log('Available Times:', availableTimes);
+            const data = await response.json();
+            if (data.restaurants && data.restaurants.length > 0) times.push(i.toString());
         }
+
+        setAvailableTimes(times);
+
+        if (times.length === 0) setMessage("No available times found");
+        else setMessage("");
+
+        setLoading(false);
     };
 
-    useEffect(() => {
-        const fetchRestaurantInfo = async () => {
-            try {
-                const response = await fetch(process.env.NEXT_PUBLIC_FUNCTION_URL + "/GetRestaurantInfo", {
-                    method: "POST",
-                    headers: {
-                        "Content-Type": "application/json",
-                    },
-                    body: JSON.stringify({ restaurantID: restaurantID }),
-                });
-                const data = await response.json();
-                setRestaurantInfo(JSON.parse(data.body));
-                setRestaurant({ name: JSON.parse(data.body).restaurantInfo.name, address: JSON.parse(data.body).restaurantInfo.address });
-            } catch (error) {
-                console.error('Error fetching restaurant info:', error);
-            }
-        };
+    async function fetchRestaurantInfo() {
+        const response = await fetch(process.env.NEXT_PUBLIC_FUNCTION_URL + "/GetRestaurantInfo", {
+            method: "POST",
+            headers: { "Content-Type": "application/json", },
+            body: JSON.stringify({ restaurantID }),
+        });
 
-        if (restaurantID) {
-            fetchRestaurantInfo();
-        }
-    }, [restaurantID]);
+        const data = await response.json();
+        setRestaurantInfo(JSON.parse(data.body));
+        setRestaurant({
+            name: JSON.parse(data.body).restaurantInfo.name,
+            address: JSON.parse(data.body).restaurantInfo.address
+        });
+    }
+    useEffect(() => { fetchRestaurantInfo() }, []);
 
     useEffect(() => {
-        if (date && guestCount && restaurantInfo.restaurantInfo.openingTime && restaurantInfo.restaurantInfo.closingTime) {
+        if (date && guestCount && restaurantInfo)
             getAvailableTimes(date, parseInt(guestCount));
-        }
     }, [date, guestCount]);
 
-    const handleSubmit = async (event: React.FormEvent) => {
+    async function handleSubmit(event: React.FormEvent) {
         event.preventDefault();
     
         // Validate email
@@ -134,10 +112,8 @@ const MakeReservation: React.FC = () => {
     
         // get submit button by ID
         // disable it
-        const submitButton = document.getElementById("SubmitButton");
-        if (submitButton) {
-            submitButton.setAttribute("disabled", "true");
-        }
+        const submitButton = document.getElementById("SubmitButton"); // oh my GOD don't do this
+        if (submitButton) submitButton.setAttribute("disabled", "true");
     
         // Handle form submission logic here
         console.log(`Name: ${name}, Email: ${email}, Time: ${time}`);
@@ -150,128 +126,88 @@ const MakeReservation: React.FC = () => {
             customerCount: guestCount,
         };
         console.log('Payload:', payload);
-        const makeReservationSubmit = async () => {
-            try {
-                const response = await fetch(process.env.NEXT_PUBLIC_FUNCTION_URL + "/MakeReservation", {
-                    method: "POST",
-                    headers: {
-                        "Content-Type": "application/json",
-                    },
-                    body: JSON.stringify(payload),
-                });
-                let data = await response.json();
-                data = JSON.parse(data.body);
-                console.log('Reservation Response:', data);
-                setReservationCode(data.confirmationCode);
-            } catch (error) {
-                console.error('Error:', error);
-            }
-        };
-    
-        if (restaurantID) {
-            await makeReservationSubmit();
-        }
-    };
 
-    useEffect(() => {
-        if (reservationCode) {
-            setHasReserved(true);
-        }
-    }, [reservationCode]);
+        const response = await fetch(process.env.NEXT_PUBLIC_FUNCTION_URL + "/MakeReservation", {
+            method: "POST",
+            headers: {
+                "Content-Type": "application/json",
+            },
+            body: JSON.stringify(payload),
+        });
+        let data = await response.json();
+        data = JSON.parse(data.body);
+        console.log('Reservation Response:', data);
+        setReservationCode(data.confirmationCode);
+    };
 
     if (!hasReserved) {
         return (
-            <Suspense fallback={<div>Loading...</div>}>
-                <div style={{ padding: '20px', maxWidth: '400px', margin: '0 auto', border: '1px solid #ccc', borderRadius: '10px' }}>
-                    <h1>Make a Reservation</h1>
-                    {/* <p>Restaurant: {restaurantID}</p> */}
-                    <p><strong>{restaurant.name}</strong></p>
-                    <p>{restaurant.address}</p>
-                    <p>4 guests &middot; Monday, November 11th</p>
-                    {/* Date Input */}
-                    <div className="find-input">
-                        <label htmlFor="date">Day: </label>
-                        <input
-                            type="date"
-                            name="date"
-                            value={date}
-                            onChange={(e) => setDate(e.target.value)}
-                        />
-                    </div>
-                    {/* Guest Count Input */}
-                    <div className="find-input">
-                        <label htmlFor="guestCount">Guest Count: </label>
-                        <select
-                            name="guestCount"
-                            value={guestCount}
-                            onChange={(e) => setGuestCount(e.target.value)}>
-                            <option value="1">1</option>
-                            <option value="2">2</option>
-                            <option value="3">3</option>
-                            <option value="4">4</option>
-                            <option value="5">5</option>
-                            <option value="6">6</option>
-                            <option value="7">7</option>
-                            <option value="8">8</option>
-                        </select>
-                    </div>
-                    {/* Name Input */}
-                    <div className="find-input">
-                        <label htmlFor="name">Name: </label>
-                        <input
-                            type="text"
-                            name="name"
-                            value={name}
-                            onChange={(e) => setName(e.target.value)}
-                            required
-                        />
-                    </div>
-                    {/* Email Input */}
-                    <div className="find-input">
-                        <label htmlFor="email">Email: </label>
-                        <input
-                            type="email"
-                            name="email"
-                            value={email}
-                            onChange={(e) => setEmail(e.target.value)}
-                            required
-                            pattern="[a-z0-9._%+-]+@[a-z0-9.-]+\.[a-z]{2,}$"
-                            title="Please enter a valid email address"
-                        />
-                    </div>
-                    <form onSubmit={handleSubmit}>
-                        <div>
-                            <label>
-                                Select a time:
-                                <div>
-                                    {loading ? (
-                                        <p>{message}</p>
-                                    ) : (
-                                        availableTimes.length > 0 ? (
-                                            availableTimes.map((availableTime) => (
-                                                <button
-                                                    type="button"
-                                                    key={availableTime}
-                                                    onClick={() => setTime(availableTime)}
-                                                    style={{
-                                                        backgroundColor: time === availableTime ? 'lightblue' : 'white'
-                                                    }}
-                                                >
-                                                    {availableTime}
-                                                </button>
-                                            ))
-                                        ) : (
-                                            <p>{message}</p>
-                                        )
-                                    )}
-                                </div>
-                            </label>
-                        </div>
-                        {/* Add other form fields here */}
-                        <button id="SubmitButton" type="submit" disabled={!name || !email || !time || !date} style={{ backgroundColor: (!name || !email || !time || !date) ? 'grey' : 'orange' }}>Reserve!</button>
-                    </form>
+            <div style={{ padding: '20px', maxWidth: '400px', margin: '0 auto', border: '1px solid #ccc', borderRadius: '10px' }}>
+                <h1>Make a Reservation</h1>
+                {/* <p>Restaurant: {restaurantID}</p> */}
+                <p><strong>{restaurant.name}</strong></p>
+                <p>{restaurant.address}</p>
+                <p>4 guests &middot; Monday, November 11th</p>
+                {/* Date Input */}
+                <div className="find-input">
+                    <label htmlFor="date">Day: </label>
+                    <input type="date" name="date" value={date}
+                        onChange={(e) => setDate(e.target.value)}
+                    />
                 </div>
-            </Suspense>
+                {/* Guest Count Input */}
+                <div className="find-input">
+                    <label htmlFor="guestCount">Guest Count: </label>
+                    <select
+                        name="guestCount"
+                        value={guestCount}
+                        onChange={(e) => setGuestCount(e.target.value)}>
+                        { [...Array(8)].map((_, i) => (<option key={i} value={i+1}>{i+1}</option>)) }
+                    </select>
+                </div>
+                {/* Name Input */}
+                <div className="find-input">
+                    <label htmlFor="name">Name: </label>
+                    <input type="text" name="name" value={name} required
+                        onChange={(e) => setName(e.target.value)}
+                    />
+                </div>
+                {/* Email Input */}
+                <div className="find-input">
+                    <label htmlFor="email">Email: </label>
+                    <input type="email" name="email" value={email} required
+                        onChange={(e) => setEmail(e.target.value)}
+                        title="Please enter a valid email address"
+                    />
+                </div>
+                <form onSubmit={handleSubmit}>
+                    <div>
+                        <p>Select a time:</p>
+                        <div>
+                            { loading && <p>{message}</p>}
+                            { !loading && availableTimes.length > 0 && 
+                                availableTimes.map((availableTime) => (
+                                    <button
+                                        type="button"
+                                        key={availableTime}
+                                        onClick={() => setTime(availableTime)}
+                                        style={{
+                                            backgroundColor: time === availableTime ? 'lightblue' : 'white'
+                                        }}
+                                    >{availableTime}</button>
+                                ))
+                            }
+                            { !loading && availableTimes.length == 0 && <p>{message}</p> }
+                        </div>
+                    </div>
+                    {/* Add other form fields here */}
+                    <button id="SubmitButton" type="submit"
+                        disabled={!name || !email || !time || !date}
+                        style={{ backgroundColor: (!name || !email || !time || !date) ? 'grey' : 'orange' }}>
+                        Reserve
+                    </button>
+                </form>
+            </div>
         );
     } else {
         return (
@@ -279,13 +215,3 @@ const MakeReservation: React.FC = () => {
         );
     }
 };
-
-function MakeReservationSuspenseWrapper() {
-    return (
-        <Suspense fallback={<div>Loading...</div>}>
-            <MakeReservation />
-        </Suspense>
-    );
-}
-
-export default MakeReservationSuspenseWrapper;
