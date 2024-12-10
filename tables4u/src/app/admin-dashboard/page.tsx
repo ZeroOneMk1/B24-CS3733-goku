e34ed625c5ca510595d17eb4233a94f13a1a73ca
notsoli <samrandaofficial@gmail.com>
2024-12-09 22:36:27 -0500

    Fixed styling and cut unused code

--- tables4u/src/app/admin-dashboard/page.tsx
+++ tables4u/src/app/admin-dashboard/page.tsx
@@ -1,59 +1,22 @@
 'use client';
 import { useState, useEffect } from "react";
-import { useRouter } from "next/navigation";
-import Restaurants from './Restaurants';
-
 import { Dashboard } from "@/components/Dashboard";
 
 export default function AdminDashboard() {
-    const router = useRouter();
     const [restaurantList, setRestaurantList] = useState<any[]>([]);
-    const [listError, setListError] = useState("Loading restaurants...");
-    const [ selectedRestaurant, setSelectedRestaurant ] = useState<string | undefined>(undefined);
-
-    const getCookie = (name: string) => document.cookie.match(new RegExp(`(^| )${name}=([^;]+)`))?.at(2);
 
     async function listAllRestaurants() {
-        setListError("Loading restaurants...");
         const url = process.env.NEXT_PUBLIC_FUNCTION_URL + "/ListAllRestaurants";
         const body = JSON.stringify({
-            jwt: getCookie("jwt")
+            jwt: document.cookie.match(new RegExp(`(^| )jwt=([^;]+)`))?.at(2)
         });
         const response = await fetch(url, { method: "POST", body });
         const result = await response.json();
-        if(result.statusCode == 200) {
-            setListError("");
-            const list = [];
-            for (const restaurant of result.restaurants) {
-                list.push(restaurant);
-            }
-            {/* 
-            // @ts-ignore the state doesn't start with a defined type other than array so react gets mad :( */}
-            setRestaurantList(list);
-        } else {
-            setRestaurantList([]);
-            setListError(result.error);
-        }
+        if (result.statusCode == 200) setRestaurantList(result.restaurants);
+        else window.alert("Error fetching restaurants: " + result.error);
     }
 
-    function logout() {
-        document.cookie = "jwt=;";
-        router.push("/");
-    }
+    useEffect(() => { listAllRestaurants() }, []);
 
-    useEffect(() => {
-        let ignore = false;
-        
-        if (!ignore)  listAllRestaurants()
-        return () => { ignore = true; }
-    },[]);
-
-    return (
-        <div id="admin-dashboard-panel">
-            <Dashboard restaurantList={restaurantList}/>
-            {/* <p id="admin-restaurant-list-error">{listError}</p>
-            <Restaurants restaurantsInfo={restaurantList}/>
-            <button onClick={logout}>Logout</button> */}
-        </div>
-    );
+    return ( <Dashboard restaurantList={restaurantList}/> );
 }
